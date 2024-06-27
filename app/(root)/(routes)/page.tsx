213d04db4be48750caaacaf05dbcc0584bc97587
RchtDshr <rachita17dashore@gmail.com>
2024-06-27 21:36:31 +0530

    companions with categoryId

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,15 +1,47 @@
 import Categories from '@/components/categories';
+import Companions from '@/components/companions';
 import SearchInput from '@/components/search-input'
 import prismadb from '@/lib/prismadb'
-import { UserButton } from '@clerk/nextjs'
-import React from 'react'
 
-export default async function RootPage() {
+interface RootPageParams{
+  // every server component has searchParams which is used to extract data from url
+  searchParams: {
+    categoryId: string,
+    name: string
+  }
+}
+
+export default async function RootPage({searchParams} : RootPageParams) {
+
+  // extract data from db with categoryId from url and show them in the screen
+  //  order the result by when it was created and also count the number of msgs from each user showcasing the popularity by msgs
+  const data = await prismadb.companion.findMany({
+    where: {
+      categoryId: searchParams.categoryId,
+      name: {
+        search: searchParams.name
+      }
+    },
+    orderBy: {
+      createdAt: 'desc'
+    },
+    include: {
+      _count: {
+        select: {
+          msgs: true
+        }
+      }
+    }
+  
+  })
+
+  // find all categories from the categories table
   const categories = await prismadb.category.findMany();
   return (
     <div className='h-full space-y-2 p-4'>
        <SearchInput />
        <Categories data={categories} />
+       <Companions data={data} />
     </div>
   )
 }
---
+++ components/companions.tsx
@@ -0,0 +1,73 @@
+import { Companion } from "@prisma/client";
+import Image from "next/image";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import Link from "next/link";
+import { MessageCircle } from "lucide-react";
+
+interface CompanionsProps {
+  // passing a list of data which conatins companion and their count
+  data: (Companion & {
+    _count: {
+      msgs: number;
+    };
+  })[];
+}
+
+export default function Companions({ data }: CompanionsProps) {
+  if (data.length == 0) {
+    return (
+      <div className="p-6 flex flex-col items-center justify-center">
+        <div className="relative w-80 h-80">
+          <Image
+            fill
+            className="grayscale"
+            src="/notfound.png"
+            alt="Not Found"
+          />
+        </div>
+        <h1 className="text-xl">No companions found</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grip-cols-5 lg:grid-cols-6 gap-2 ">
+      {data.map((item) => (
+        <Card
+          key={item.id}
+          className="bg-primary/10 py-5 cursor-pointer hover:opacity-75 transition border-3 pb-0"
+        >
+          <Link href={`/chat/${item.id}`}>
+            <CardHeader className="flex flex-col justify-center items-center text-left gap-3 pb-2">
+              <div className="relative rounded h-[10rem] w-[10rem] ">
+                <Image fill className="rounded-md object-cover" src={item.src} alt={`${item.name}`} />
+              </div>
+              {/* <CardTitle> */}
+                <p className="text-left text-lg w-full font-bold"> {item.name}</p>
+              {/* </CardTitle> */}
+            </CardHeader>
+            <CardFooter className="flex justify-between items-center text-xs text-muted-foreground">
+                <div className="lowercase">
+                @{item.userName}
+                </div>
+                <div className="flex items-center gap-1 ">
+                  
+                        <MessageCircle className="w-4" />
+                  
+                  
+                        {item._count.msgs}
+                  
+                </div>
+            </CardFooter>
+          </Link>
+        </Card>
+      ))}
+    </div>
+  );
+}
